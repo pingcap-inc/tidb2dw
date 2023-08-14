404bceea6787f953ca31eea5bf5e153bec0d0fd1
Lloyd-Pottiger <60744015+Lloyd-Pottiger@users.noreply.github.com>
2023-08-14 17:44:37 +0800

    Bump version to v0.0.2 (#47)
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gmail.com>

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ var (
 	// TiDB2DWVerMinor is the minor version of TiDB2DW
 	TiDB2DWVerMinor = 0
 	// TiDB2DWVerPatch is the patch version of TiDB2DW
-	TiDB2DWVerPatch = 1
+	TiDB2DWVerPatch = 2
 	// TiDB2DWVerName is an alternative name of the version
 	TiDB2DWVerName = "TiDB2DW"
 	// GitHash is the current git commit hash
