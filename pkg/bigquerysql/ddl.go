b67731bf12d2eaa7f39ac89310d8165dc4cb381b
WD <me@wangdi.ink>
2024-01-23 10:20:58 +0800

    bigquery: add identifier escapes when generate sql & support unsigned (#94)

--- pkg/bigquerysql/ddl.go
+++ pkg/bigquerysql/ddl.go
@@ -21,18 +21,18 @@ func GetColumnModifyString(diff *tidbsql.ColumnDiff) (string, error) {
 			return "", errors.Trace(err)
 		}
 		// https://cloud.google.com/bigquery/docs/reference/standard-sql/conversion_rules
-		strs = append(strs, fmt.Sprintf("%s SET DATA TYPE %s", diff.After.Name, colType))
+		strs = append(strs, fmt.Sprintf("`%s` SET DATA TYPE %s", diff.After.Name, colType))
 	}
 	if diff.Before.Default != diff.After.Default {
 		if diff.After.Default == nil {
-			strs = append(strs, fmt.Sprintf("%s DROP DEFAULT", diff.After.Name))
+			strs = append(strs, fmt.Sprintf("`%s` DROP DEFAULT", diff.After.Name))
 		} else {
-			strs = append(strs, fmt.Sprintf("%s SET DEFAULT %s", diff.After.Name, getDefaultString(diff.After.Default)))
+			strs = append(strs, fmt.Sprintf("`%s` SET DEFAULT %s", diff.After.Name, getDefaultString(diff.After.Default)))
 		}
 	}
 	if diff.Before.Nullable != diff.After.Nullable {
 		if diff.After.Nullable == "true" {
-			strs = append(strs, fmt.Sprintf("%s DROP NOT NULL", diff.After.Name))
+			strs = append(strs, fmt.Sprintf("`%s` DROP NOT NULL", diff.After.Name))
 		} else {
 			log.Warn("BigQuery does not support update column required", zap.String("column", diff.After.Name), zap.Any("before", diff.Before.Nullable), zap.Any("after", diff.After.Nullable))
 		}
@@ -44,10 +44,10 @@ func GenDDLViaColumnsDiff(datasetID, tableID string, prevColumns []cloudstorage.
 	tableFullName := fmt.Sprintf("%s.%s", datasetID, tableID)
 
 	if curTableDef.Type == timodel.ActionTruncateTable {
-		return []string{fmt.Sprintf("TRUNCATE TABLE %s", tableFullName)}, nil
+		return []string{fmt.Sprintf("TRUNCATE TABLE `%s`", tableFullName)}, nil
 	}
 	if curTableDef.Type == timodel.ActionDropTable {
-		return []string{fmt.Sprintf("DROP TABLE %s", tableFullName)}, nil
+		return []string{fmt.Sprintf("DROP TABLE `%s`", tableFullName)}, nil
 	}
 	if curTableDef.Type == timodel.ActionCreateTable {
 		return nil, errors.New("Received create table ddl, which should not happen") // FIXME: drop table and create table
@@ -72,7 +72,7 @@ func GenDDLViaColumnsDiff(datasetID, tableID string, prevColumns []cloudstorage.
 		ddl := ""
 		switch item.Action {
 		case tidbsql.ADD_COLUMN:
-			ddl += fmt.Sprintf("ALTER TABLE %s ADD COLUMN ", tableFullName)
+			ddl += fmt.Sprintf("ALTER TABLE `%s` ADD COLUMN ", tableFullName)
 			colStr, err := GetBigQueryColumnString(*item.After, false)
 			if err != nil {
 				return nil, errors.Trace(err)
@@ -82,17 +82,17 @@ func GenDDLViaColumnsDiff(datasetID, tableID string, prevColumns []cloudstorage.
 			if item.After.Default != nil {
 				ddls = append(
 					ddls,
-					fmt.Sprintf("ALTER TABLE %s ALTER COLUMN %s SET DEFAULT %s;", tableFullName, item.After.Name, getDefaultString(item.After.Default)),
-					fmt.Sprintf("UPDATE %s SET %s = %s WHERE TRUE;", tableFullName, item.After.Name, getDefaultString(item.After.Default)),
+					fmt.Sprintf("ALTER TABLE `%s` ALTER COLUMN `%s` SET DEFAULT %s;", tableFullName, item.After.Name, getDefaultString(item.After.Default)),
+					fmt.Sprintf("UPDATE `%s` SET `%s` = %s WHERE TRUE;", tableFullName, item.After.Name, getDefaultString(item.After.Default)),
 				)
 
 			} else if item.After.Nullable == "true" {
-				ddls = append(ddls, fmt.Sprintf("ALTER TABLE %s ALTER COLUMN %s SET DEFAULT NULL;", tableFullName, item.After.Name))
+				ddls = append(ddls, fmt.Sprintf("ALTER TABLE `%s` ALTER COLUMN `%s` SET DEFAULT NULL;", tableFullName, item.After.Name))
 			}
 		case tidbsql.DROP_COLUMN:
-			ddls = append(ddls, fmt.Sprintf("ALTER TABLE %s DROP COLUMN %s;", tableFullName, item.Before.Name))
+			ddls = append(ddls, fmt.Sprintf("ALTER TABLE `%s` DROP COLUMN `%s`;", tableFullName, item.Before.Name))
 		case tidbsql.MODIFY_COLUMN:
-			ddl += fmt.Sprintf("ALTER TABLE %s ALTER COLUMN ", tableFullName)
+			ddl += fmt.Sprintf("ALTER TABLE `%s` ALTER COLUMN ", tableFullName)
 			modifyStr, err := GetColumnModifyString(&item)
 			if err != nil {
 				return nil, errors.Trace(err)
@@ -129,7 +129,7 @@ func GetBigQueryColumnString(column cloudstorage.TableCol, createTable bool) (st
 	if err != nil {
 		return "", errors.Trace(err)
 	}
-	sb.WriteString(fmt.Sprintf("%s %s", column.Name, colType))
+	sb.WriteString(fmt.Sprintf("`%s` %s", column.Name, colType))
 	if createTable {
 		if column.Default != nil {
 			sb.WriteString(fmt.Sprintf(` DEFAULT %s`, getDefaultString(column.Default)))
