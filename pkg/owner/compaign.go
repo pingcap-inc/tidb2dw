6c3a92a7a496afbb8bd67e6f74120d48e4495d16
zhangjinpeng87 <zzzhangjinpeng@gmail.com>
2024-03-27 14:25:33 -0700

    micro-service: init owner service
    Signed-off-by: zhangjinpeng87 <zzzhangjinpeng@gmail.com>

---
+++ pkg/owner/compaign.go
@@ -0,0 +1,69 @@
+package owner
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+	"time"
+
+	"github.com/pingcap-inc/tidb2dw/pkg/owner/config"
+	"github.com/pingcap-inc/tidb2dw/pkg/owner/meta"
+	"golang.org/x/sync/errgroup"
+)
+
+type Compaign struct {
+	cfg *config.OwnerConfig
+
+	backend meta.Backend
+
+	isOwner atomic.Bool
+}
+
+func NewCompaign(cfg *config.OwnerConfig, backend meta.Backend) *Compaign {
+	return &Compaign{
+		cfg:     cfg,
+		backend: backend,
+		isOwner: atomic.Bool{},
+	}
+}
+
+func (c *Compaign) Start(eg *errgroup.Group, ctx context.Context) {
+	eg.Go(func() error {
+		serverId := fmt.Sprintf("%s:%d", c.cfg.Host, c.cfg.Port)
+
+		ticker := time.NewTicker(time.Duration(c.cfg.LeaseRenewInterval) * time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				if c.IsOwner() {
+					success, err := c.backend.RenewOwnerLease(serverId, c.cfg.LeaseDuration)
+					if err != nil {
+						// log error
+						continue
+					}
+					if !success {
+						// Someone else has taken the owner
+						c.isOwner.Store(false)
+					}
+				} else {
+					// Try to campaign owner
+					success, err := c.backend.TryCompaignOwner(serverId, c.cfg.LeaseDuration)
+					if err != nil {
+						// log error
+						continue
+					}
+					if success {
+						c.isOwner.Store(true)
+					}
+				}
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	})
+}
+
+func (c *Compaign) IsOwner() bool {
+	return c.isOwner.Load()
+}
